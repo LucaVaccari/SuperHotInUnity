06629c7f4a311c7e2a28441f9ba3515771a5fed1
LucaVaccari <lucavaccari03@gmail.com>
2020-07-05 11:30:16 +0200

    Project

---
+++ SuperHotUnityProject/Assets/Scripts/Core/Player/PlayerMove.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float speed = 10;
+    [SerializeField] private float xSens = 10, ySens = 10;
+
+    Vector2 moveInput, lookInput;
+
+    CharacterController cc;
+
+    private void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        CalculateInput();
+        Look();
+        Move();
+    }
+
+    private void CalculateInput()
+    {
+        moveInput.x = Input.GetAxis("Horizontal");
+        moveInput.y = Input.GetAxis("Vertical");
+
+        lookInput.x = Input.GetAxis("Mouse X");
+        lookInput.y = Input.GetAxis("Mouse Y");
+    }
+    private void Look()
+    {
+
+    }
+    private void Move()
+    {
+        Vector3 velocity = new Vector3(moveInput.x, 0, moveInput.y) * Time.unscaledDeltaTime * speed;
+        cc.Move(velocity);
+    }
+}
