5b98a201b2a58f74a27b5b40aa595b1a0d3a6ed6
LucaVaccari <lucavaccari03@gmail.com>
2020-07-07 10:35:12 +0200

    Bullet

--- SuperHotUnityProject/Assets/Scripts/Weapons/Bullet.cs
+++ SuperHotUnityProject/Assets/Scripts/Weapons/Bullet.cs
@@ -4,6 +4,22 @@ namespace Digital.Weapons
 {
     public class Bullet : MonoBehaviour
     {
+        [SerializeField] float force;
 
+        Rigidbody rb;
+
+        [HideInInspector] public Vector3 direction;
+
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+
+            rb.AddForce(direction * force * Time.fixedUnscaledDeltaTime, ForceMode.Force);
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- SuperHotUnityProject/Assets/Scripts/Weapons/Gun.cs
+++ SuperHotUnityProject/Assets/Scripts/Weapons/Gun.cs
@@ -1,16 +1,23 @@
-﻿using UnityEngine;
+﻿using Digital.Core;
+using UnityEngine;
 
 namespace Digital.Weapons
 {
     public class Gun : MonoBehaviour, IWeapon
     {
+        [SerializeField] GameObject bulletPrefab;
+
         public MonoBehaviour MonoBehaviour => this;
 
         public bool Throwed { get; set; }
 
         public void Action()
         {
-            //shoot
+            //play shoot animation
+            //play shoot sound
+            //instantiate particles
+            GameObject bulletIns = Instantiate(bulletPrefab, GameManager.ins.cam.position, GameManager.ins.cam.rotation);
+            bulletIns.GetComponent<Bullet>().direction = GameManager.ins.cam.forward;
         }
 
         private void OnCollisionEnter(Collision collision)
